55df5068e5d158cf5b99748f52ea40d95880896a
First <yhm>
2021-05-08 13:21:03 +0200

    Method generated random world was added. Together with that some useful constants were added too.

--- world/world.py
+++ world/world.py
@@ -27,7 +27,6 @@ class World(object):
 
         self.generate_world()
 
-
     def generate_world(self):
         """ generate random world with the agent, obstacles and target point """
         agent_x = randrange(0, self.__width)
@@ -37,7 +36,12 @@ class World(object):
         for i in range(OBSTACLES_COUNT):
             obstacle_x = randrange(0, self.__width)
             obstacle_y = randrange(0, self.__height)
-            self.__world[obstacle_y][obstacle_x] = OBSTACLE_FIELD_VALUE
+
+            for h in range(-MAX_OBSTACLE_HEIGHT//2, MAX_OBSTACLE_HEIGHT//2):
+                for w in range(-MAX_OBSTACLE_WIDTH//2, MAX_OBSTACLE_WIDTH//2):
+                    if 0 <= obstacle_x + w < self.__width and 0 <= obstacle_y + h < self.__height:
+                        if random() >= OBSTACLE_PROBABILITY and self.__world[obstacle_y+h][obstacle_x+w] == EMPTY_FIELD_VALUE:
+                            self.__world[obstacle_y + h][obstacle_x + w] = OBSTACLE_FIELD_VALUE
 
         destination_x = randrange(0, self.__width)
         destination_y = randrange(0, self.__height)
@@ -54,19 +58,18 @@ class World(object):
         if y - 1 >= 0:
             for i in range(-1, 2, 1):
                 if 0 <= x + i < self.__width:
-                    env_vector[i+1] = self.__world[y-1][x+i]
+                    env_vector[i + 1] = self.__world[y - 1][x + i]
 
         for i in range(-1, 2, 1):
             if 0 <= x + i < self.__width:
-                env_vector[i+4] = self.__world[y][x+i]
+                env_vector[i + 4] = self.__world[y][x + i]
 
         if y + 1 < self.__height:
             for i in range(-1, 2, 1):
                 if 0 <= x + i < self.__width:
-                    env_vector[i+7] = self.__world[y+1][x+i]
+                    env_vector[i + 7] = self.__world[y + 1][x + i]
 
         return env_vector
-
 
     def __str__(self):
         """ define how world should be shown as string """
@@ -85,7 +88,6 @@ class World(object):
             world_as_string += row + '\n'
         return world_as_string
 
-
     def set_width(self, width: int):
         """ Set new world width """
         assert width > 0, 'World width has to be positive value'
@@ -102,6 +104,12 @@ class World(object):
         assert 0 <= point[1] < self.__height, 'Agent point has bad coordinates'
         self.__agent_x = point[0]
         self.__agent_y = point[1]
+
+    def get_point(self, x: int, y: int):
+        """ Return point from game world """
+        assert 0 <= x < self.__width, 'Point beyond the world'
+        assert 0 <= y < self.__height, 'Point beyond the world'
+        return self.__world[y][x]
 
     def get_agent_point(self):
         """ Return agent point coordinates as a tuple """
